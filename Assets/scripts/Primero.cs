e1c425d704f9573ee7d0f326d009a9c351ab8528
Chispas25 <kanet11beach2003@gmail.com>
2025-03-21 14:39:11 +0100

    personaje y suelo colisionan

--- Assets/scripts/Primero.cs
+++ Assets/scripts/Primero.cs
@@ -5,19 +5,9 @@ using UnityEngine;
 public class Primero : MonoBehaviour
 {
     // Start is called before the first frame update
-
-    int miEdad = 21;
-
-    float miAltura = 4.1f;
-
-    string miNombre = "Khar";
-
-    bool estoyCalvo = false;
-
-
     void Start()
     {
-       Debug.Log($"Hola mi nombre es {miNombre} y tengo {miEdad} años.");
+       
     }
 
     // Update is called once per frame
